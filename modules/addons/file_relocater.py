628e2595b6e4ecf1d8bd26d24f1b78cad4dda8b8
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2025-08-06 14:23:13 -0500

    changed default structure to /album/year/album artist

--- modules/addons/file_relocater.py
+++ modules/addons/file_relocater.py
@@ -8,7 +8,7 @@ Licensed under the BSD-3-Clause License (see LICENSE file for details)
 A tool to move audio files into folder structures based on metadata.
 Moves files from a source library into organized subfolders under a specified root directory.
 
-Default folder structure: /(album)/(albumartist)/ with sanitized folder names but can be changed by user.
+Default folder structure: /(album)/(year)/(albumartist)/ with sanitized folder names but can be changed by user.
 """
 
 import os
@@ -37,7 +37,7 @@ ALLOWED_FOLDER_CHARS = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 AUDIO_EXTENSIONS = {'.mp3', '.flac', '.wav', '.ogg', '.m4a', '.aac', '.opus', '.wma', '.ape', '.wv'}
 
 # Default folder structure format
-DEFAULT_FOLDER_FORMAT = "{album}/{albumartist}"
+DEFAULT_FOLDER_FORMAT = "{album}/{year}/{albumartist}"
 
 # Default character replacements (applied before other sanitization)
 DEFAULT_CHAR_REPLACEMENTS = {'/': '-', '\\': '-', ':': '-', '|': '-'}
